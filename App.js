61bea867708e7c75784e08a8317f7a6c2439b7b8
Baochuan Lu <lubaochuan@gmail.com>
2023-04-13 09:45:44 -0500

    add Profile screen

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Contacts from './screens/Contacts';
+
+import Profile from './screens/Profile';
 
 export default function App() {
-  return <Contacts />;
+  return <Profile />;
 }
