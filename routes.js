641ab5ce9e4cb8288641fd5df62bc05940af0923
Baochuan Lu <lubaochuan@gmail.com>
2023-04-13 13:16:35 -0500

    customize screen title and header style via navigation options

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import Contacts from './screens/Contacts';
 import Profile from './screens/Profile';
+import colors from './utils/colors';
 
 const Root = createNativeStackNavigator()
 
@@ -10,8 +11,27 @@ export default function AppContainer() {
   return (
     <NavigationContainer>
       <Root.Navigator>
-        <Root.Screen name="Contacts" component={Contacts} />
-        <Root.Screen name="Profile" component={Profile} />
+        <Root.Screen
+          name="Contacts"
+          component={Contacts}
+          options={{
+            title: 'Contacts',
+            headerStyle: {
+              backgroundColor: 'white',
+            }
+          }}
+        />
+        <Root.Screen
+          name="Profile"
+          component={Profile}
+          options={({ route }) => {
+            return {
+              title: route.params.contact.name.split(' ')[0],
+              headerStyle: {
+                backgroundColor: colors.blue,
+              }
+            }}}
+          />
       </Root.Navigator>
     </NavigationContainer>
   )
