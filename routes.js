a5fab7009f7c0e97f11e093946a57fd7770e92ac
Baochuan Lu <lubaochuan@gmail.com>
2023-04-14 10:51:59 -0500

    add a tab navigator and test it out

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Contacts from './screens/Contacts';
 import Profile from './screens/Profile';
@@ -9,8 +10,44 @@ import User from './screens/User';
 import colors from './utils/colors';
 
 const Root = createNativeStackNavigator()
+const Tab = createBottomTabNavigator()
 
 export default function AppContainer() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName="Contacts">
+        <Tab.Screen
+          name="Contacts"
+          component={Contacts}
+          options={{
+            title: 'Contacts',
+            headerStyle: {
+              backgroundColor: 'white',
+            }
+          }}
+        />
+        <Tab.Screen
+          name="Favorites"
+          component={Favorites}
+        />
+        <Tab.Screen
+          name="User"
+          component={User}
+          options={({ route }) => {
+            return {
+              title: 'Me',
+              headerTintColor: 'white',
+              headerStyle: {
+                backgroundColor: colors.blue,
+              },
+            }}}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
+
+/* export default function AppContainer() {
   return (
     <NavigationContainer>
       <Root.Navigator initialRouteName="User">
@@ -55,3 +92,4 @@ export default function AppContainer() {
     </NavigationContainer>
   )
 }
+ */
