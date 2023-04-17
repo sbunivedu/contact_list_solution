416a9c356cd3c7b7abe060d396f5c6682a5c8ae9
Baochuan Lu <lubaochuan@gmail.com>
2023-04-17 10:14:28 -0500

    add stack navigators to tab navigator

--- routes.js
+++ routes.js
@@ -15,96 +15,139 @@ const getTabBarIcon = icon => ({ tintColor }) => (
   <MaterialIcons name={icon} size={26} style={{ color: tintColor }} />
 );
 
-const Root = createNativeStackNavigator()
 const Tab = createBottomTabNavigator()
 
 export default function AppContainer() {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="Contacts">
+      <Tab.Navigator
+        initialRouteName="ContactScreens"
+        screenOptions={{
+          tabBarLabelStyle: {
+            display: "none" // hide screen labels on tab bar
+          },
+        }}>
         <Tab.Screen
-          name="Contacts"
-          component={Contacts}
+          name="ContactsScreens"
+          component={ContactsScreens}
           options={{
-            title: 'Contacts',
-            headerStyle: {
-              backgroundColor: 'white',
-            },
             tabBarIcon: getTabBarIcon('list'),
+            headerShown: false
           }}
         />
         <Tab.Screen
-          name="Favorites"
-          component={Favorites}
+          name="FavoritesScreens"
+          component={FavoritesScreens}
           options={{
-            title: 'Favorites',
-            headerStyle: {
-              backgroundColor: 'white',
-            },
             tabBarIcon: getTabBarIcon('star'),
+            headerShown: false
           }}
         />
         <Tab.Screen
-          name="User"
-          component={User}
-          options={({ route }) => {
-            return {
-              title: 'Me',
-              headerTintColor: 'white',
-              headerStyle: {
-                backgroundColor: colors.blue,
-              },
-              tabBarIcon: getTabBarIcon('person'),
-            }}}
+          name="UserScreen"
+          component={UserScreen}
+          options={{
+            tabBarIcon: getTabBarIcon('person'),
+            headerShown: false
+          }}
         />
       </Tab.Navigator>
     </NavigationContainer>
   )
 }
 
-/* export default function AppContainer() {
+const contactsStack = createNativeStackNavigator()
+
+function ContactsScreens() {
   return (
-    <NavigationContainer>
-      <Root.Navigator initialRouteName="User">
-        <Root.Screen
-          name="Contacts"
-          component={Contacts}
-          options={{
-            title: 'Contacts',
+    <contactsStack.Navigator
+      initialRouteName="Contacts"
+      options={{
+        tabBarIcon: getTabBarIcon('list'),
+      }}>
+      <contactsStack.Screen
+        name="Contacts"
+        component={Contacts}
+        options={{
+          title: 'Contacts',
+          headerStyle: {
+            backgroundColor: 'white',
+          }
+        }}
+      />
+      <contactsStack.Screen
+        name="Profile"
+        component={Profile}
+        options={({ route }) => {
+          return {
+            title: route.params.contact.name.split(' ')[0],
             headerStyle: {
-              backgroundColor: 'white',
+              backgroundColor: colors.blue,
             }
-          }}
-        />
-        <Root.Screen
-          name="Profile"
-          component={Profile}
-          options={({ route }) => {
-            return {
-              title: route.params.contact.name.split(' ')[0],
-              headerStyle: {
-                backgroundColor: colors.blue,
-              }
-            }}}
-          />
-        <Root.Screen
-          name="Favorites"
-          component={Favorites}
-        />
-        <Root.Screen
-          name="User"
-          component={User}
-          options={({ route }) => {
-            return {
-              title: 'Me',
-              headerTintColor: 'white',
-              headerStyle: {
-                backgroundColor: colors.blue,
-              },
-            }}}
-        />
-      </Root.Navigator>
-    </NavigationContainer>
+          }
+        }}
+      />
+    </contactsStack.Navigator>
   )
 }
- */
+
+const favoritesStack = createNativeStackNavigator()
+
+function FavoritesScreens() {
+  return (
+    <favoritesStack.Navigator
+      initialRouteName="Favorites"
+      options={{
+        tabBarIcon: getTabBarIcon('star'),
+      }}>
+      <favoritesStack.Screen
+        name="Favorites"
+        component={Favorites}
+        options={{
+          title: 'Favorites',
+          headerStyle: {
+            backgroundColor: 'white',
+          }
+        }}
+      />
+      <favoritesStack.Screen
+        name="Profile"
+        component={Profile}
+        options={({ route }) => {
+          return {
+            title: route.params.contact.name.split(' ')[0],
+            headerStyle: {
+              backgroundColor: colors.blue,
+            }
+          }
+        }}
+      />
+    </favoritesStack.Navigator>
+  )
+}
+
+const userStack = createNativeStackNavigator()
+
+function UserScreen() {
+  return (
+    <userStack.Navigator
+      initialRouteName="User"
+      options={{
+        tabBarIcon: getTabBarIcon('person'),
+      }}>
+      <userStack.Screen
+        name="User"
+        component={User}
+        options={({ route }) => {
+          return {
+            title: 'Me',
+            headerTintColor: 'white',
+            headerStyle: {
+              backgroundColor: colors.blue,
+            },
+          }
+        }}
+      />
+    </userStack.Navigator>
+  )
+}
